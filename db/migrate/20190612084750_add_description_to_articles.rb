db2200d9e4f8780abfae5a6d602cc6326b8d9e08
Omkar Deshpande <omkard199@gmail.com>
2019-09-07 12:29:51 +0530

    reload

--- db/migrate/20190612084750_add_description_to_articles.rb
+++
@@ -1,7 +0,0 @@
-class AddDescriptionToArticles < ActiveRecord::Migration[5.2]
-  def change
-	  add_column :articles, :description, :text #name of table, name of attribute,type
-	  add_column :articles, :created_at, :datetime
-	  add_column :articles, :updated_at, :datetime
-  end
-end
